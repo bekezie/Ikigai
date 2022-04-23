d7e0e0218a8b0d7fa9a8f641df177b502464141d
Austin-Chow <imr.chow@gmail.com>
2022-04-23 15:32:52 -0400

    Added BottomNavigation bar with fragments: Home, Entries, Exercise, and More. When the User clicks on the buttons in the BottomNavigation bar, it leads the User to the fragment.

--- app/src/main/java/edu/neu/ikigai/HomeScreen.java
+++ app/src/main/java/edu/neu/ikigai/HomeScreen.java
@@ -1,8 +1,14 @@
 package edu.neu.ikigai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -10,5 +16,36 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        NavigationBarView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(navListener);
     }
+
+    private NavigationBarView.OnItemSelectedListener navListener =
+        new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_entries:
+                        selectedFragment = new EntriesFragment();
+                        break;
+                    case R.id.nav_exercise:
+                        selectedFragment = new ExerciseFragment();
+                        break;
+                    case R.id.nav_more:
+                        selectedFragment = new MoreFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+
+                return true;
+            }
+        };
 }
