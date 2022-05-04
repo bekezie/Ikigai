4a2023c0f21db60ad4d237f44ecbceb14d47e5aa
Austin-Chow <imr.chow@gmail.com>
2022-05-03 21:22:05 -0400

    The user now navigates to the 'WorksheetSummary' activity whenever they click on a worksheet entry card in RecyclerView.

--- app/src/main/java/edu/neu/ikigai/EntriesHistory/EntriesHistoryRecyclerHolder.java
+++ app/src/main/java/edu/neu/ikigai/EntriesHistory/EntriesHistoryRecyclerHolder.java
@@ -1,5 +1,6 @@
 package edu.neu.ikigai.EntriesHistory;
 
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,12 @@ import android.widget.TextView;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import edu.neu.ikigai.BottomNavigationBarFragments;
+import edu.neu.ikigai.LoginActivity;
 import edu.neu.ikigai.R;
+import edu.neu.ikigai.WorksheetSummary;
+
+import static androidx.core.content.ContextCompat.startActivity;
 
 public class EntriesHistoryRecyclerHolder extends RecyclerView.ViewHolder {
 
@@ -28,7 +34,10 @@ public class EntriesHistoryRecyclerHolder extends RecyclerView.ViewHolder {
         worksheetEntryCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("EntriesHistoryRecyclerHolder", "Clicked: " + worksheetEntryNumber.getText());
+                // Log.d("EntriesHistoryRecyclerHolder", "Clicked: " + worksheetEntryNumber.getText());
+
+                Intent summaryIntent = new Intent(entryCardView.getContext(), WorksheetSummary.class);
+                entryCardView.getContext().startActivity(summaryIntent);
             }
         });
     }
