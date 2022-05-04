4a62f77ec2ee8cd7d86f6be951b535cd4fc61411
Austin-Chow <imr.chow@gmail.com>
2022-05-03 23:25:38 -0400

    Saving state when device rotation.

--- app/src/main/java/edu/neu/ikigai/BottomNavigationBarFragments.java
+++ app/src/main/java/edu/neu/ikigai/BottomNavigationBarFragments.java
@@ -18,6 +18,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import edu.neu.ikigai.Fragments.EntriesFragment;
@@ -28,6 +29,8 @@ import edu.neu.ikigai.Fragments.MoreFragment;
 
 public class BottomNavigationBarFragments extends AppCompatActivity {
 
+    private int fragmentNumber = 1;
+
     private NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -36,15 +39,22 @@ public class BottomNavigationBarFragments extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.nav_home:     // Home Fragment
                     selectedFragment = new HomeNoSummaryFragment();
+                    fragmentNumber = 1;
                     break;
                 case R.id.nav_entries:  // Entries Fragment
                     selectedFragment = new EntriesFragment();
+                    fragmentNumber = 2;
                     break;
                 case R.id.nav_exercise: // Exercise Fragment
                     selectedFragment = new ExerciseFragment();
+                    fragmentNumber = 3;
                     break;
                 case R.id.nav_more:     // More Fragment
                     selectedFragment = new MoreFragment();
+                    fragmentNumber = 4;
+                    break;
+                default:
+                    selectedFragment = new HomeNoSummaryFragment();
                     break;
             }
 
@@ -66,5 +76,39 @@ public class BottomNavigationBarFragments extends AppCompatActivity {
         getSupportFragmentManager()
             .beginTransaction()
             .replace(R.id.fragment_container, new HomeNoSummaryFragment()).commit();
+
+        // onRestoreInstanceState
+        if (savedInstanceState != null) {
+
+            int savedFragmentNumber = savedInstanceState.getInt("fragmentNumber");
+            Fragment selectedFragment = null;
+
+            switch (savedFragmentNumber) {
+                case 1:     // Home Fragment
+                    selectedFragment = new HomeNoSummaryFragment();
+                    break;
+                case 2:  // Entries Fragment
+                    selectedFragment = new EntriesFragment();
+                    break;
+                case 3: // Exercise Fragment
+                    selectedFragment = new ExerciseFragment();
+                    break;
+                case 4:     // More Fragment
+                    selectedFragment = new MoreFragment();
+                    break;
+                default:
+                    selectedFragment = new HomeNoSummaryFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("fragmentNumber", fragmentNumber);
     }
 }
